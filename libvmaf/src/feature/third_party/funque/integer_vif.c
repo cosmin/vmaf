92516f3a4c37e472154e1b44f47cc84725932555
Rakshith G B <luckydudefeelinggreat@gmail.com>
2022-06-29 15:24:46 +0530

    vif changes

--- libvmaf/src/feature/third_party/funque/integer_vif.c
+++ libvmaf/src/feature/third_party/funque/integer_vif.c
@@ -225,7 +225,7 @@ void integer_compute_metrics(const int64_t* int_1_x, const int64_t* int_1_y, con
     }
 }
 
-int compute_vif_funque(const dwt2_dtype* x_t, const dwt2_dtype* y_t, const funque_dtype* x, const funque_dtype* y, size_t width, size_t height, double* score, double* score_num, double* score_den, int k, int stride, double sigma_nsq, int64_t shift_val)
+int integer_compute_vif_funque(const dwt2_dtype* x_t, const dwt2_dtype* y_t, const funque_dtype* x, const funque_dtype* y, size_t width, size_t height, double* score, double* score_num, double* score_den, int k, int stride, double sigma_nsq, int64_t shift_val)
 {
     int ret = 1;
 
