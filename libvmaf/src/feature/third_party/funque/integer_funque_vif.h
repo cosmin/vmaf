651db6ffccd63f5fdbe7f6d1fa7e4d0a04b16ab0
Ajay Shyam <ajayshyam@ittiam.com>
2022-08-23 10:44:40 +0530

    Variable name clean-up in funque vif

--- libvmaf/src/feature/third_party/funque/integer_funque_vif.h
+++ libvmaf/src/feature/third_party/funque/integer_funque_vif.h
@@ -43,66 +43,64 @@ static inline uint32_t get_best_u18_from_u64(uint64_t temp, int *power)
 static inline vif_stats_calc(int32_t int_1_x, int32_t int_1_y, 
                              int64_t int_2_x, int64_t int_2_y, int64_t int_x_y, 
                              int16_t knorm_fact, int16_t knorm_shift, int k_norm, 
-                             int16_t exp_t, int32_t sigma_nsq_t, uint32_t *log_18,
-                             int64_t *score_num_t, int64_t *num_power,
-                             int64_t *score_den_t, int64_t *den_power)
+                             int16_t exp_t, int32_t sigma_nsq, uint32_t *log_18,
+                             int64_t *score_num, int64_t *num_power,
+                             int64_t *score_den, int64_t *den_power)
 {
     int32_t mx = int_1_x;
     int32_t my = int_1_y;
     int32_t var_x = (int_2_x - (((int64_t) mx * mx * knorm_fact) >> knorm_shift)) >> VIF_COMPUTE_METRIC_R_SHIFT;
-    int32_t var_y_t = (int_2_y - (((int64_t) my * my * knorm_fact) >> knorm_shift)) >> VIF_COMPUTE_METRIC_R_SHIFT;
-    int32_t cov_xy_t = (int_x_y - (((int64_t) mx * my * knorm_fact) >> knorm_shift)) >> VIF_COMPUTE_METRIC_R_SHIFT;
+    int32_t var_y = (int_2_y - (((int64_t) my * my * knorm_fact) >> knorm_shift)) >> VIF_COMPUTE_METRIC_R_SHIFT;
+    int32_t cov_xy = (int_x_y - (((int64_t) mx * my * knorm_fact) >> knorm_shift)) >> VIF_COMPUTE_METRIC_R_SHIFT;
 
     if (var_x < exp_t)
     {
         var_x = 0;
-        cov_xy_t = 0;
+        cov_xy = 0;
     }
     
-    if (var_y_t < exp_t)
+    if (var_y < exp_t)
     {
-        var_y_t = 0;
-        cov_xy_t = 0;
+        var_y = 0;
+        cov_xy = 0;
     }
-    int32_t g_t_num = cov_xy_t;
+    int32_t g_num = cov_xy;
     int32_t g_den = var_x + exp_t*k_norm;
 
-    int32_t sv_sq_t = (var_y_t - ((int64_t)g_t_num * cov_xy_t)/g_den);
+    int32_t sv_sq = (var_y - ((int64_t)g_num * cov_xy)/g_den);
 
 
-    if((g_t_num < 0 && g_den > 0) || (g_den < 0 && g_t_num > 0))
+    if((g_num < 0 && g_den > 0) || (g_den < 0 && g_num > 0))
     {
-        sv_sq_t = var_x;
-        g_t_num = 0;
+        sv_sq = var_x;
+        g_num = 0;
     }
 
-    if (sv_sq_t < (exp_t * k_norm))
-        sv_sq_t = exp_t * k_norm;
+    if (sv_sq < (exp_t * k_norm))
+        sv_sq = exp_t * k_norm;
 
-    int64_t p1 = ((int64_t)g_t_num * g_t_num)/g_den;
+    int64_t p1 = ((int64_t)g_num * g_num)/g_den;
     int32_t p2 = var_x;
-    int64_t n1 = p1 * p2;
-    int64_t n2 = g_den * ((int64_t) sv_sq_t + sigma_nsq_t);
-    int64_t num_t = n2 + n1;
-    int64_t num_den_t = n2;
+    int64_t n2 = g_den * ((int64_t) sv_sq + sigma_nsq);
+    int64_t n1 = n2 + (p1 * p2);
     int x1, x2;
 
-    uint32_t log_in_num_1 = get_best_u18_from_u64((uint64_t)num_t, &x1);
-    uint32_t log_in_num_2 = get_best_u18_from_u64((uint64_t)num_den_t, &x2);
-    int32_t temp_numerator = (int64_t)log_18[log_in_num_1] - (int64_t)log_18[log_in_num_2];
+    uint32_t log_in_num_1 = get_best_u18_from_u64((uint64_t)n1, &x1);
+    uint32_t log_in_num_2 = get_best_u18_from_u64((uint64_t)n2, &x2);
+    int64_t temp_numerator = (int64_t)log_18[log_in_num_1] - (int64_t)log_18[log_in_num_2];
     int32_t temp_power_num = x1 - x2; 
-    *score_num_t += temp_numerator;
+    *score_num += temp_numerator;
     *num_power += temp_power_num;
 
-    uint32_t d1 = ((uint32_t)sigma_nsq_t + (uint32_t)(var_x));
-    uint32_t d2 = (sigma_nsq_t);
+    uint32_t d1 = ((uint32_t)sigma_nsq + (uint32_t)(var_x));
+    uint32_t d2 = (sigma_nsq);
     int y1, y2;
 
     uint32_t log_in_den_1 = get_best_u18_from_u64((uint64_t)d1, &y1);
     uint32_t log_in_den_2 = get_best_u18_from_u64((uint64_t)d2, &y2);
-    int32_t temp_denominator =  (int64_t)log_18[log_in_den_1] - (int64_t)log_18[log_in_den_2];
+    int64_t temp_denominator =  (int64_t)log_18[log_in_den_1] - (int64_t)log_18[log_in_den_2];
     int32_t temp_power_den = y1 - y2;
-    *score_den_t += temp_denominator;
+    *score_den += temp_denominator;
     *den_power += temp_power_den;
 }
 
@@ -110,11 +108,11 @@ static inline vif_stats_calc(int32_t int_1_x, int32_t int_1_y,
 //numerator denominator score calculations are done
 static inline vif_horz_integralsum(int kw, int width_p1, 
                                    int16_t knorm_fact, int16_t knorm_shift, int k_norm, 
-                                   int16_t exp_t, int32_t sigma_nsq_t, uint32_t *log_18,
+                                   int16_t exp_t, int32_t sigma_nsq, uint32_t *log_18,
                                    int32_t *interim_1_x, int32_t *interim_1_y,
                                    int64_t *interim_2_x, int64_t *interim_2_y, int64_t *interim_x_y,
-                                   int64_t *score_num_t, int64_t *num_power,
-                                   int64_t *score_den_t, int64_t *den_power)
+                                   int64_t *score_num, int64_t *num_power,
+                                   int64_t *score_den, int64_t *den_power)
 {
     int32_t int_1_x, int_1_y;
     int64_t int_2_x, int_2_y, int_x_y;
@@ -148,8 +146,8 @@ static inline vif_horz_integralsum(int kw, int width_p1,
      */
     vif_stats_calc(int_1_x, int_1_y, int_2_x, int_2_y, int_x_y,
                     knorm_fact, knorm_shift, k_norm, 
-                    exp_t, sigma_nsq_t, log_18,
-                    score_num_t, num_power, score_den_t, den_power);
+                    exp_t, sigma_nsq, log_18,
+                    score_num, num_power, score_den, den_power);
 
     //Similar to prev loop, but previous kw col interim metric sum is subtracted
     for (size_t j=kw+1; j<width_p1; j++)
@@ -165,8 +163,8 @@ static inline vif_horz_integralsum(int kw, int width_p1,
 
         vif_stats_calc(int_1_x, int_1_y, int_2_x, int_2_y, int_x_y,
                         knorm_fact, knorm_shift, k_norm, 
-                        exp_t, sigma_nsq_t, log_18,
-                        score_num_t, num_power, score_den_t, den_power);
+                        exp_t, sigma_nsq, log_18,
+                        score_num, num_power, score_den, den_power);
     }
 
 }
