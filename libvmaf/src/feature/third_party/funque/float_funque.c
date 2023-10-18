0226cb9ea69cf76c14dd6d7ad7bd47e541e768f0
Mallikarjun Kamble <mallikarjun.kamble@ittiam.com>
2023-10-18 19:17:39 +0530

    Revert changes

--- libvmaf/src/feature/third_party/funque/float_funque.c
+++ libvmaf/src/feature/third_party/funque/float_funque.c
@@ -101,7 +101,7 @@ static const VmafOption options[] = {
         .help = "Enable resize for funque",
         .offset = offsetof(FunqueState, enable_resize),
         .type = VMAF_OPT_TYPE_BOOL,
-        .default_val.b = false,
+        .default_val.b = true,
     },
     {
         .name = "enable_spatial_csf",
@@ -110,6 +110,7 @@ static const VmafOption options[] = {
         .offset = offsetof(FunqueState, enable_spatial_csf),
         .type = VMAF_OPT_TYPE_BOOL,
         .default_val.b = false,
+        .flags = VMAF_OPT_FLAG_FEATURE_PARAM,
     },
     {
         .name = "num_taps",
