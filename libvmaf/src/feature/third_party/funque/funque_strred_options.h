1352ec7a9f8ea2dc83102b1cc1f198c94f36f2cf
Mallikarjun Kamble <mallikarjun.kamble@ittiam.com>
2023-12-27 19:51:29 +0530

    Added strred files

---
+++ libvmaf/src/feature/third_party/funque/funque_strred_options.h
@@ -0,0 +1,35 @@
+/**
+ *
+ *  Copyright 2016-2020 Netflix, Inc.
+ *
+ *     Licensed under the BSD+Patent License (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         https://opensource.org/licenses/BSDplusPatent
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *
+ */
+
+#pragma once
+
+#ifndef STRRED_OPTIONS_H_
+#define STRRED_OPTIONS_H_
+
+#define STRRED_SIGMA_NSQ        0.1
+#define STRRED_WINDOW_SIZE      9
+
+#define PI_CONSTANT             3.1415926535897932384626433832795028841971693993751
+#define EULERS_CONSTANT         2.71828182845904523536028747135266249775724709369995
+
+#define BLOCK_SIZE              1
+
+#define DEFAULT_STRRED_LEVELS   4
+#define DEFAULT_STRRED_SUBBANDS 4
+
+#endif /* STRRED_OPTIONS_H_ */
